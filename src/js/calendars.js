7e13a8c6ac885a8b589645f44ee5e27ebc06f362
Azraul <dahkaros@gmail.com>
2020-05-27 12:56:10 +0300

    cleaned

--- src/js/calendars.js
+++ src/js/calendars.js
@@ -40,21 +40,6 @@ function makeCalendars(selection) {
     document.getElementById("submitDates").addEventListener("click", () => checkAPIrequest(selection));
 }
 
-function updateCalendars(target, selection) {
-    let date = new Date(target.value);
-    const dateLimits = {
-        "Weekly": 7,
-        "Daily": 1,
-        "Hourly": 24 * 60 * 60 * 1000
-    };
-    /*console.log("as date: " + target.valueAsDate);
-    console.log("as number: " + target.valueAsNumber);
-    if (selection == "Hourly") {
-        date.setTime(date.getTime() + dateLimits[selection]);
-        document.getElementById("endCalendar").value = new Date(date).toISOString().split("T")[0];
-    }*/
-}
-
 function constructCalendar(id, selection) {
     const calendar = document.createElement("input");
     if (selection == "Weekly") {
@@ -69,7 +54,6 @@ function constructCalendar(id, selection) {
         calendar.max = new Date(date).toISOString().split("T")[0];
     }
     calendar.setAttribute("id", id);
-    calendar.addEventListener("change", (e) => updateCalendars(e.target, selection));
     divAppend(calendar);
 }
 
@@ -113,16 +97,16 @@ function clearCalendars() {
 
 function checkAPIrequest(selection) {
     const range = selection.value
-    const dateLimits = {
+    const corrections = {
         "Weekly": (7 * 24 * 60 * 60 * 1000),
         "Daily": (24 * 60 * 60 * 1000),
         "Hourly": (24 * 60 * 60 * 1000)
     };
     const select = selection.options[selection.selectedIndex].text;
-    let checkAPI = true;    // Turn false on problems
-    let timeStamp;          // The final timestamp for api call
-    let timeStamps = [];    // Push calendars value here
-    let type = "";          // calendar type
+    let checkAPI = true;                // Turn false on problems
+    let timeStamp;                      // The final timestamp for api call
+    let timeStamps = [];                // Push calendars value here
+    let type = setType(select);         // calendar type
     if (select == "Weekly") {
         type = "week";
     } else {
@@ -150,26 +134,18 @@ function checkAPIrequest(selection) {
     console.log("timestamps[1]", timeStamps[1], "as date obj", new Date(timeStamps[1]));
     console.log("Range choosen: " + range);
     if (checkAPI) {
-        const correction = dateLimits[select] -1;
-        console.log("correction: "+correction);
-        const max = Math.max.apply(null, timeStamps) + correction; // Lägger till 23h59m59s om daily
+        const correction = corrections[select] - 1; // -1 fpr Crypto API
+        console.log("correction: " + correction);
+        const max = Math.max.apply(null, timeStamps) + correction;
         const min = Math.min.apply(null, timeStamps);
-        console.log("Date: "+new Date().getTime());
-        console.log("max: "+max);
-        console.log("min: "+min);
+        console.log("Date: " + new Date().getTime());
+        console.log("max: " + max);
+        console.log("min: " + min);
         if (!max || !min) {
             checkAPI = false;
         }
         let limit = Math.floor((max - min) / (1000 * range));
-        timeStamp = Math.floor(max/1000);
-        /*if (select == "Weekly") {
-            timeStamp += 1000 * 60 * 60 * 24 * 7; // Moves pointer to end of week
-            limit -= 1;                           // Fix: Goes over into next day
-        } else if (select == "Daily") {
-            timeStamp += 86399000;                // Moves pointer to end of day
-        } else if (select == "Hourly"){
-            limit -= 1;                           // Fix: Goes over into next day
-        }*/
+        timeStamp = Math.floor(max / 1000);
         console.log("timeresolution: " + select)
         console.log("timestamp: " + timeStamp);
         console.log("limit: " + limit);
@@ -177,4 +153,22 @@ function checkAPIrequest(selection) {
 
         makeApiCall(select, timeStamp, limit, "ETH");
     }
+}
+
+function setType(select) {
+    let type;
+    if (select == "Weekly") {
+        type = "week";
+    } else if (select == "Daily") {
+        type = "date";
+    }
+    else if (select == "Hourly") {
+        type = "date";
+        setEndCalendar();
+    }
+    return type;
+}
+function setEndCalendar() {
+    let date = new Date(document.getElementById("startCalendar").value);
+    document.getElementById("endCalendar").value = new Date(date).toISOString().split("T")[0];
 }
